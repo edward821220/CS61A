a5b93e5d62ff697d94ed8de42615a87bb0b93331
Edward <edward821220@gmail.com>
2023-06-06 10:14:16 +0800

    lab14

---
+++ lab/lab14/lab14.py
@@ -0,0 +1,219 @@
+from operator import add, sub, mul
+
+
+def prune_min(t):
+    """Prune the tree mutatively from the bottom up.
+
+    >>> t1 = Tree(6)
+    >>> prune_min(t1)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_min(t2)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(3, [Tree(1), Tree(2)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_min(t3)
+    >>> t3
+    Tree(6, [Tree(3, [Tree(1)])])
+    """
+    "*** YOUR CODE HERE ***"
+    if Tree.is_leaf(t):
+        return
+
+    if len(t.branches) == 2:
+        t.branches = (
+            [t.branches[1]]
+            if t.branches[0].label > t.branches[1].label
+            else [t.branches[0]]
+        )
+
+    prune_min(t.branches[0])
+
+
+def num_splits(s, d):
+    """Return the number of ways in which s can be partitioned into two
+    sublists that have sums within d of each other.
+
+    >>> num_splits([1, 5, 4], 0)  # splits to [1, 4] and [5]
+    1
+    >>> num_splits([6, 1, 3], 1)  # no split possible
+    0
+    >>> num_splits([-2, 1, 3], 2) # [-2, 3], [1] and [-2, 1, 3], []
+    2
+    >>> num_splits([1, 4, 6, 8, 2, 9, 5], 3)
+    12
+    """
+    "*** YOUR CODE HERE ***"
+
+    def helper(l1, l2, prev_all):
+        if not len(l1):
+            return 1 if abs(sum(l1) + prev_all - sum(l2)) <= d else 0
+
+        if abs(sum(l1) + prev_all - sum(l2)) <= d:
+            return helper(l1[1:], l2 + [l1[0]], prev_all) + helper(
+                l1[1:], l2, prev_all + l1[0]
+            )
+
+        return helper(l1[1:], l2 + [l1[0]], prev_all) + helper(
+            l1[1:], l2, prev_all + l1[0]
+        )
+
+    return helper(s, [], 0) // 2
+
+
+class Account(object):
+    """A bank account that allows deposits and withdrawals.
+
+    >>> eric_account = Account('Eric')
+    >>> eric_account.deposit(1000000)   # depositing my paycheck for the week
+    1000000
+    >>> eric_account.transactions
+    [('deposit', 1000000)]
+    >>> eric_account.withdraw(100)      # buying dinner
+    999900
+    >>> eric_account.transactions
+    [('deposit', 1000000), ('withdraw', 100)]
+    """
+
+    interest = 0.02
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        """Increase the account balance by amount and return the
+        new balance.
+        """
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Decrease the account balance by amount and return the
+        new balance.
+        """
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance = self.balance - amount
+        return self.balance
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ", " + repr(self.branches)
+        else:
+            branch_str = ""
+        return "Tree({0}{1})".format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = "  " * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+
+        return print_tree(self).rstrip()
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ", " + repr(self.rest)
+        else:
+            rest_repr = ""
+        return "Link(" + repr(self.first) + rest_repr + ")"
+
+    def __str__(self):
+        string = "<"
+        while self.rest is not Link.empty:
+            string += str(self.first) + " "
+            self = self.rest
+        return string + str(self.first) + ">"
